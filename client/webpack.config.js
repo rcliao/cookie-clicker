cc7f1fa2177d741347686fca0f46a24f9f95496e
Eric Liao <rcliao01@gmail.com>
2018-02-13 23:52:20 -0800

    chore: adjust webpack destination

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
 	entry: './src/app.js',
 	output: {
 		filename: 'app.bundle.js',
-		path: path.join(__dirname, '..', 'build')
+		path: path.join(__dirname, '..', 'dest')
 	},
 	devtool: 'source-map',
 	module: {
