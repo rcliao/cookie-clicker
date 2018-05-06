53482a8bec96d82d09d55da8084a7821965b6716
Eric Liao <rcliao01@gmail.com>
2018-05-05 18:11:59 -0700

    feat: add guestbook example

--- src/main/java/edu/csula/web/HelloJsonServlet.java
+++ src/main/java/edu/csula/web/HelloJsonServlet.java
@@ -2,6 +2,8 @@ package edu.csula.web;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.*;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -19,11 +21,18 @@ public class HelloJsonServlet extends HttpServlet {
 		response.setContentType("application/json");
 		PrintWriter out = response.getWriter();
 
+		List<User> users = new ArrayList<>();
 		User u = new User(0, "username", "password");
+		User u2 = new User(1, "eric", "notmypassword");
+		users.add(u);
+		users.add(u2);
 		GsonBuilder builder = new GsonBuilder();
 		Gson gson = builder.create();
-		String jsonString = gson.toJson(u);
+		String jsonString = gson.toJson(users);
 
-		out.println(jsonString);
+		request.setAttribute("state", jsonString);
+
+		request.getRequestDispatcher("WEB-INF/json.jsp")
+			.forward(request, response);
 	}
 }
